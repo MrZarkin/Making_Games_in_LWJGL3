03712b9a4a6fd7b5ea352ea7099fb4c6e4ba056b
Zarkin <matryck@outlook.fr>
2026-07-06 10:57:21 +0200

    feat: Adding Inputs Managment

--- GameProject/src/engine/io/Window.java
+++ GameProject/src/engine/io/Window.java
@@ -2,74 +2,167 @@ package engine.io;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.glfw.GLFWWindowSizeCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
 
-public class Window {
-    
-    private int height, width;
-    private String title;
-    private long window;
-    public int frames;
-    public static long time;
-
-    public Window(int height, int width, String title)
+public class Window
+{
+	private int width, height;
+	private String title;
+	private long window;
+	private int frames;
+	private static long time;
+	private Input input;
+	private float backgroundR, backgroundG, backgroundB;
+	private GLFWWindowSizeCallback sizeCallback;
+	private boolean isResized;
+	private boolean isFullscreen;
+	private int[] windowPosX = new int[1], windowPosY = new int[1];
+	
+	public Window(int width, int height, String title)
     {
-        this.height = height;
-        this.width = width;
-        this.title = title;
-    }
-
-    public void create()
+		this.width = width;
+		this.height = height;
+		this.title = title;
+	}
+	
+	public void create()
     {
-        // Init glfw
-        if(!GLFW.glfwInit())
+		if (!GLFW.glfwInit())
         {
-            System.out.println("ERR: Failed to load GLFW");
-            return;
-        }
-
-        // Create window
-        window = GLFW.glfwCreateWindow(width, height, title, 0, 0);
-
-        if(window == 0)
+			System.err.println("ERROR: GLFW wasn't initializied");
+			return;
+		}
+		
+		input = new Input();
+		window = GLFW.glfwCreateWindow(width, height, title, isFullscreen ? GLFW.glfwGetPrimaryMonitor() : 0, 0);
+		
+		if (window == 0)
         {
-            System.out.println("ERR: Failed to create Window");
-            return;
-        }
-
-        // Get primart monitor informations (width, height, refresh rate...) and center the window
-        GLFWVidMode videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
-		GLFW.glfwSetWindowPos(window, (videoMode.width() - width) / 2, (videoMode.height() - height) / 2);
+			System.err.println("ERROR: Window wasn't created");
+			return;
+		}
+		
+		GLFWVidMode videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+		windowPosX[0] = (videoMode.width() - width) / 2;
+		windowPosY[0] = (videoMode.height() - height) / 2;
+		GLFW.glfwSetWindowPos(window, windowPosX[0], windowPosY[0]);
 		GLFW.glfwMakeContextCurrent(window);
-
-        GLFW.glfwShowWindow(window);
-
-        // VSync
-        GLFW.glfwSwapInterval(1);
-
-        time = System.currentTimeMillis();
-    }
-
-    public void update()
+		GL.createCapabilities();
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		
+		createCallbacks();
+		
+		GLFW.glfwShowWindow(window);
+		
+		GLFW.glfwSwapInterval(1);
+		
+		time = System.currentTimeMillis();
+	}
+	
+	private void createCallbacks()
     {
-        GLFW.glfwPollEvents();
-        frames++;
-
+		sizeCallback = new GLFWWindowSizeCallback()
+        {
+			public void invoke(long window, int w, int h)
+            {
+				width = w;
+				height = h;
+				isResized = true;
+			}
+		};
+		
+		GLFW.glfwSetKeyCallback(window, input.getKeyboardCallback());
+		GLFW.glfwSetCursorPosCallback(window, input.getMouseMoveCallback());
+		GLFW.glfwSetMouseButtonCallback(window, input.getMouseButtonsCallback());
+		GLFW.glfwSetScrollCallback(window, input.getMouseScrollCallback());
+		GLFW.glfwSetWindowSizeCallback(window, sizeCallback);
+	}
+	
+	public void update()
+    {
+		if (isResized)
+        {
+			GL11.glViewport(0, 0, width, height);
+			isResized = false;
+		}
+		GL11.glClearColor(backgroundR, backgroundG, backgroundB, 1.0f);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		GLFW.glfwPollEvents();
+		frames++;
 		if (System.currentTimeMillis() > time + 1000)
         {
 			GLFW.glfwSetWindowTitle(window, title + " | FPS: " + frames);
 			time = System.currentTimeMillis();
 			frames = 0;
 		}
-    }
-
-    public void swapBuffers()
+	}
+	
+	public void swapBuffers()
     {
-        GLFW.glfwSwapBuffers(window);
-    }
-
-    public boolean shouldClose()
+		GLFW.glfwSwapBuffers(window);
+	}
+	
+	public boolean shouldClose()
     {
-        return GLFW.glfwWindowShouldClose(window);
-    }
+		return GLFW.glfwWindowShouldClose(window);
+	}
+	
+	public void destroy()
+    {
+		input.destroy();
+		sizeCallback.free();
+		GLFW.glfwWindowShouldClose(window);
+		GLFW.glfwDestroyWindow(window);
+		GLFW.glfwTerminate();
+	}
+	
+	public void setBackgroundColor(float r, float g, float b)
+    {
+		backgroundR = r;
+		backgroundG = g;
+		backgroundB = b;
+	}
 
+	public boolean isFullscreen()
+    {
+		return isFullscreen;
+	}
+
+	public void setFullscreen(boolean isFullscreen)
+    {
+		this.isFullscreen = isFullscreen;
+		isResized = true;
+
+		if (isFullscreen)
+        {
+			GLFW.glfwGetWindowPos(window, windowPosX, windowPosY);
+			GLFW.glfwSetWindowMonitor(window, GLFW.glfwGetPrimaryMonitor(), 0, 0, width, height, 0);
+		}
+        else
+        {
+			GLFW.glfwSetWindowMonitor(window, 0, windowPosX[0], windowPosY[0], width, height, 0);
+		}
+	}
+
+	public int getWidth()
+    {
+		return width;
+	}
+
+	public int getHeight()
+    {
+		return height;
+	}
+
+	public String getTitle()
+    {
+		return title;
+	}
+
+	public long getWindow()
+    {
+		return window;
+	}
 }
--- GameProject/src/main/Main.java
+++ GameProject/src/main/Main.java
@@ -1,49 +1,62 @@
 package main;
 
+import org.lwjgl.glfw.GLFW;
+
+import engine.io.Input;
 import engine.io.Window;
 
 public class Main implements Runnable
 {
-    public Thread game;
-    public static Window window;
-    public static final int WIDTH = 1280, HEIGHT = 760;
 
-    public void start()
+	public Thread game;
+	public Window window;
+	public final int WIDTH = 1280, HEIGHT = 760;
+	
+	public void start()
     {
-        game = new Thread(this, "game");
-        game.start();
-    }
-
-    public void init()
+		game = new Thread(this, "game");
+		game.start();
+	}
+	
+	public void init()
     {
-        System.out.println("The game is starting");
-        window = new Window(HEIGHT, WIDTH, "Game Name");
-        window.create();
-    }
-
-    public void run()
+		window = new Window(WIDTH, HEIGHT, "Game");
+		window.setBackgroundColor(1.0f, 0, 0);
+		window.create();
+	}
+	
+	public void run()
     {
-        init();
-        while(!window.shouldClose())
+		init();
+		while(!window.shouldClose() && !Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE))
         {
-            update();
-            render();
+			update();
+			render();
+			if(Input.isKeyDown(GLFW.GLFW_KEY_F11))
+            {
+                window.setFullscreen(!window.isFullscreen());
+            }
+		}
+		window.destroy();
+	}
+	
+	private void update()
+    {
+		window.update();
+		if(Input.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)) 
+        {
+            System.out.println("X: " + Input.getScrollX() + ", Y: " + Input.getScrollY());
+	
         }
     }
 
-    public void update()
+	private void render()
     {
-        window.update();
-    }
-
-    public void render()
+		window.swapBuffers();
+	}
+	
+	public static void main(String[] args)
     {
-        window.swapBuffers();
-    }
-
-
-    public static void main(String[] args)
-    {
-        new Main().start();
-    }
+		new Main().start();
+	}
 }
